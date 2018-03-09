213e2cd93dfd12f4f61a18a65f623a02a96ec2ad
babausse <courtois.vincent@outlook.com>
2018-03-09 12:14:28 +0100

    Use the new shared examples for service testing

--- spec/controllers/services_spec.rb
+++ spec/controllers/services_spec.rb
@@ -41,53 +41,7 @@ RSpec.describe Controllers::Services do
         })
       end
     end
-    describe 'bad request errors' do
-      describe 'no token error' do
-        before do
-          get '/', {app_key: 'test_key'}
-        end
-        it 'Raises a bad request (400) error when the parameters don\'t contain the token of the gateway' do
-          expect(last_response.status).to be 400
-        end
-        it 'returns the correct response if the parameters do not contain a gateway token' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'bad_request'})
-        end
-      end
-      describe 'no application key error' do
-        before do
-          get '/', {token: 'test_token'}
-        end
-        it 'Raises a bad request (400) error when the parameters don\'t contain the application key' do
-          expect(last_response.status).to be 400
-        end
-        it 'returns the correct response if the parameters do not contain a application key' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'bad_request'})
-        end
-      end
-    end
-    describe 'not_found errors' do
-      describe 'application not found' do
-        before do
-          get '/', {token: 'test_token', app_key: 'another_key'}
-        end
-        it 'Raises a not found (404) error when the key doesn\'t belong to any application' do
-          expect(last_response.status).to be 404
-        end
-        it 'returns the correct body when the gateway doesn\'t exist' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'application_not_found'})
-        end
-      end
-      describe 'gateway not found' do
-        before do
-          get '/', {token: 'other_token', app_key: 'test_key'}
-        end
-        it 'Raises a not found (404) error when the gateway does\'nt exist' do
-          expect(last_response.status).to be 404
-        end
-        it 'returns the correct body when the gateway doesn\'t exist' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'gateway_not_found'})
-        end
-      end
-    end
+
+    it_should_behave_like 'a route', 'get', '/'
   end
 end
