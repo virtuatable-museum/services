51e1f4b84eecd11f188b3ac3f74e64f3fd571c11
Babausse <courtois.vincent@outlook.com>
2018-08-04 18:21:52 +0200

    Put the service in conformity for local tests

--- controllers/services.rb
+++ controllers/services.rb
@@ -2,6 +2,9 @@ module Controllers
   # Controller for the rights, mapped on /rights
   # @author Vincent Courtois <courtois.vincent@outlook.com>
   class Services < Arkaan::Utils::Controller
+
+    load_errors_from __FILE__
+    
     declare_route 'get', '/' do
       services = Decorators::Service.decorate_collection(Arkaan::Monitoring::Service.all)
       halt 200, {count: Arkaan::Monitoring::Service.count, items: services.map(&:to_h)}.to_json
