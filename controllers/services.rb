eb1f2171a098fa2874758ceaeeaa9e26cb1b003f
Vincent <courtois.vincent@outlook.com>
2019-12-05 00:29:27 +0100

    Change the version of the gem to include the new helpers

--- controllers/services.rb
+++ controllers/services.rb
@@ -5,39 +5,36 @@ module Controllers
   # @author Vincent Courtois <courtois.vincent@outlook.com>
   class Services < Virtuatable::Controllers::Base
     api_route 'get', '/' do
-      raw_services = Arkaan::Monitoring::Service.all
-      services = Decorators::Service.decorate_collection(raw_services)
-      count = Arkaan::Monitoring::Service.count
-      halt 200, { count: count, items: services.map(&:to_h) }.to_json
+      api_list Decorators::Service.decorate_collection(all_services)
     end
 
     api_route 'get', '/:id' do
-      halt 200, Decorators::Service.new(service!).to_h.to_json
+      api_item Decorators::Service.new(service!)
     end
 
     api_route 'put', '/:id' do
       ::Services::Update.instance.update_service(service!, params)
-      halt 200, { message: 'updated' }.to_json
+      api_ok 'updated'
     end
 
     api_route 'put', '/:id/instances/:updated_instance' do
       ::Services::Update.instance.update_instance(instance!, params)
-      halt 200, { message: 'updated' }.to_json
+      api_ok 'updated'
     end
 
     api_route 'put', '/:id/routes/:route_id' do
       ::Services::Update.instance.update_route(api_route!, params)
-      halt 200, { message: 'updated' }.to_json
+      api_ok 'updated'
     end
 
     api_route 'delete', '/:id/instances/:updated_instance' do
       instance!.delete
-      halt 200, { message: 'deleted' }.to_json
+      api_ok 'deleted'
     end
 
     api_route 'delete', '/:id/routes/:route_id' do
       api_route!.delete
-      halt 200, { message: 'deleted' }.to_json
+      api_ok 'deleted'
     end
 
     def service!
@@ -56,6 +53,10 @@ module Controllers
       instance = service!.instances.where(id: params['updated_instance']).first
       api_not_found 'instance_id.unknown' if instance.nil?
       instance
+    end
+
+    def all_services
+      Arkaan::Monitoring::Service.all
     end
   end
 end
